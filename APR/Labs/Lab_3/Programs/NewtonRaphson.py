d9845706a568c83a9b1f4767211f914293f74e3b
MislavJaksic <jaksicmislav@gmail.com>
2017-11-14 11:10:49 +0100

    Implemented NewtonRaphson

--- APR/Labs/Lab_3/Programs/NewtonRaphson.py
+++ APR/Labs/Lab_3/Programs/NewtonRaphson.py
@@ -3,69 +3,85 @@ from Helpers.Matrix import Matrix
 
 import math
 
-def NewtonRaphson(startingPoint, GoalFunction, FirstPartialDerivativeFunctions, SecondPartialDerivativeFunctions, useGolden=True, epsilon=((0.1)**6)):
-  x = startingPoint
-  F = GoalFunction
-  dF = FirstPartialDerivativeFunctions
-  ddF = SecondPartialDerivativeFunctions #[[], []] format
+def NewtonRaphson(startingPoint, GoalFunction, FirstPartialDerivativeFunctions, HessianPartialDerivativeFunctions, useGolden=True, epsilon=((0.1)**6)):
+  x, F, dF, ddF = _MapToMathsNames(startingPoint, GoalFunction, FirstPartialDerivativeFunctions, HessianPartialDerivativeFunctions)
   
-  gradientMatrix = _GetGradientMatrixAtPoint(dF, x)
-  gradientNorm = _GetGradientNorm(gradientMatrix)
-  inverseHessianMatrix = _GetInverseHessianMatrixAtPoint(ddF, x)
+  gradient = _CreateGradientAtPoint(dF, x)
+  gradientNorm = _CreateGradientNorm(gradient)
+  inverseHessian = _CreateInverseHessianAtPoint(ddF, x)
   
   divergenceCounter = 0
-  bestValue = 10**6 #a sentinel value
+  bestValue = 10**6
   while _IsGradientNormLarge(gradientNorm, epsilon):
   
     if _IsDiverging(x, F, bestValue, epsilon):
       divergenceCounter += 1
     else:
-      bestValue = F(x)
+      bestValue = F(x._GetMatrixColumn(1))
     if divergenceCounter > 100:
       print "Divergence limit has been reached"
       break
       
-    KMinimum = 0.1
+    KMinimum = 1
     if useGolden == True:
       KStartingValue = (0, 1)
+      direction = _CreateDescentDirection( inverseHessian * gradient )
       singleDimensionF = _CreateOneDimensionFunction(F, x, direction)
       
       KMinimum = GoldenSectionSearch(KStartingValue, singleDimensionF, epsilon, doUnimodal=True)
       
-    for i in range(len(x)):
-      x[i] += KMinimum*direction[i]
+      x = _GoldenChangePoint(x, direction, KMinimum)
+    else:
+      x = _ChangePoint(x, inverseHessian, gradient, KMinimum)
       
-    _PrintDescent(gradientNorm, direction, x)
+    _PrintNewtonRaphson(gradientNorm, inverseHessian, x)
     
-    gradient = _GetGradientAtPoint(dF, x)
-    gradientNorm = _GetGradientNorm(gradient)
-    direction = _GetDescentDirection(gradient)
+    gradient = _CreateGradientAtPoint(dF, x)
+    gradientNorm = _CreateGradientNorm(gradient)
+    inverseHessian = _CreateInverseHessianAtPoint(ddF, x)
       
   return x
+  
+def _MapToMathsNames(startingPoint, GoalFunction, FirstPartialDerivativeFunctions, HessianPartialDerivativeFunctions):
+  """startingPoint is a Python list of numbers;
+     GoalFunction is a function saved in a variable (higher order function);
+     FirstPartialDerivativeFunctions is a Python list of functions;
+     HessianPartialDerivativeFunctions is a Python list of lists of functions;"""
+  x = _CreateColumnMatrix(startingPoint)
+  F = GoalFunction
+  dF = FirstPartialDerivativeFunctions
+  ddF = HessianPartialDerivativeFunctions
+  return x, F, dF, ddF
+  
+def _CreateColumnMatrix(list):
+  columnMatrix = []
+  for value in list:
+    columnMatrix.append( [value] )
     
-def _GetGradientMatrixAtPoint(dF, x):
+  return Matrix(columnMatrix)
+    
+def _CreateGradientAtPoint(dF, x):
   gradient = []
   for partialDerivativeFunction in dF:
-    result = partialDerivativeFunction(x)
+    result = partialDerivativeFunction(x._GetMatrixColumn(1))
     gradient.append(  [result] )
     
   return Matrix(gradient)
 
-def _GetGradientNorm(gradientMatrix):
-  gradient = gradientMatrix._GetMatrixColumn(1)
+def _CreateGradientNorm(gradient):
   sum = 0
-  for partialDerivative in gradient:
+  for partialDerivative in gradient._GetMatrixColumn(1):
     sum += partialDerivative**2
     
   return math.sqrt(sum)
 
-def _GetInverseHessianMatrixAtPoint(ddF, x):
+def _CreateInverseHessianAtPoint(ddF, x):
   hessianValues = []
   for derivativeList in ddF:
     
     results = []
     for derivative in derivativeList:
-      results.append(derivative(x))
+      results.append(derivative(x._GetMatrixColumn(1)))
     hessianValues.append(results)
     
   hessianMatrix = Matrix(hessianValues)
@@ -76,19 +92,28 @@ def _IsGradientNormLarge(gradientNorm, epsilon):
   if epsilon < gradientNorm:
     return True
   return False
-  
+
 def _IsDiverging(x, F, bestValue, epsilon):
+  x = x._GetMatrixColumn(1)
   if (bestValue - epsilon) <= F(x):
     return True
   return False
+  
+def _CreateDescentDirection(gradient):
+  descentDirection = []
+  gradientNorm = _CreateGradientNorm(gradient)
+  for value in gradient._GetMatrixColumn(1):
+    descentDirection.append( [value / ((-1) * gradientNorm)] )
+    
+  return Matrix(descentDirection)
   
 def _CreateOneDimensionFunction(compositeFunction, KPoint, KVector):
   """baseF(x) = x[0]**2           + x[1]**2 is given KPoint = [1 2] and KVector = [3 4]
      compositeF(k) = (1 + 3*k)**2 + (2 + 4*k)**2 is the resulting function"""
   def interdictor(value):
     currentCompositePoint = []
-    for i in range(len(KPoint)):
-      currentCompositePoint.append(_KFunction(KPoint[i], KVector[i]*value))
+    for i in range(len(KPoint._GetMatrixColumn(1))):
+      currentCompositePoint.append(_KFunction(KPoint[i+1,1], KVector[i+1,1]*value))
     
     calculation = compositeFunction(currentCompositePoint)
     return calculation
@@ -98,12 +123,23 @@ def _CreateOneDimensionFunction(compositeFunction, KPoint, KVector):
 def _KFunction(a, b):
   return a + b 
   
-def _PrintDescent(gradientNorm, direction, x):
+def _GoldenChangePoint(x, direction, KMinimum):
+  distance = direction.scale(KMinimum)
+    
+  return (x + distance)
+
+def _ChangePoint(x, inverseHessian, gradient, KMinimum):
+  distance = inverseHessian * gradient
+  distance = distance.scale(KMinimum)
+    
+  return (x - distance)
+
+def _PrintNewtonRaphson(gradientNorm, inverseHessianMatrix, x):
   print "-.-.-.-.-.-.-.-.-.-"
   print "gradientNorm:",
   print gradientNorm
-  print "direction:",
-  print direction
+  print "inverseHessianMatrix:",
+  print inverseHessianMatrix
   print "x:",
   print x
   
--- APR/Labs/Lab_3/Programs/Tests/test_NewtonRaphson.py
+++ APR/Labs/Lab_3/Programs/Tests/test_NewtonRaphson.py
@@ -3,40 +3,68 @@ import Tasks.TaskFunctions
 from Programs.Helpers.Matrix import Matrix
 
 import pytest
+from copy import copy
   
-def test_NewtonRaphson_GetGradientMatrixAtPoint():
+def test_NewtonRaphson_CreateColumnMatrix():
+  list = [3, 5]
+  result = Matrix([[3],
+                   [5],
+                  ])
+  assert Programs.NewtonRaphson._CreateColumnMatrix(list) == result
+  
+def test_NewtonRaphson_CreateGradientAtPoint():
   dF = [Tasks.TaskFunctions.df2x0, Tasks.TaskFunctions.df2x1]
-  x = [3, 5]
+  x = Programs.NewtonRaphson._CreateColumnMatrix([3, 5])
   result = Matrix([[-2],
                    [24],
                   ])
-  assert Programs.NewtonRaphson._GetGradientMatrixAtPoint(dF, x) == result
+  assert Programs.NewtonRaphson._CreateGradientAtPoint(dF, x) == result
   
-def test_NewtonRaphson_GetInverseHessianMatrixAtPoint():
+def test_NewtonRaphson_CreateGradientNorm():
+  gradientMatrix = Matrix([[3],
+                           [4],
+                          ])
+  assert Programs.NewtonRaphson._CreateGradientNorm(gradientMatrix) == 5
+  
+def test_NewtonRaphson_CreateInverseHessianAtPoint():
   ddF = [[Tasks.TaskFunctions.ddf2x0x0, Tasks.TaskFunctions.ddf2x0x1],
          [Tasks.TaskFunctions.ddf2x0x1, Tasks.TaskFunctions.ddf2x1x1],
         ]
-  x = [3, 5]
+  x = Programs.NewtonRaphson._CreateColumnMatrix([3, 5])
   result = Matrix([[0.5, 0    ],
                    [0,   0.125],
                   ])
-  assert Programs.NewtonRaphson._GetInverseHessianMatrixAtPoint(ddF, x) == result
+  assert Programs.NewtonRaphson._CreateInverseHessianAtPoint(ddF, x) == result
   
-# def test_NewtonRaphson_GetDescentDirection():
-  # gradient = [-8, -16]
-  # result = Programs.NewtonRaphson._GetDescentDirection(gradient)
-  # assert (0.4 < result[0]) and (result[0] < 0.5) and (0.8 < result[1]) and (result[1] < 0.9)
+def test_NewtonRaphson_CreateDescentDirection():
+  inverseHessian = Matrix([[0.5, 0],
+                           [0,   0.125],
+                          ])
+  gradient = Matrix([[6],
+                     [0],
+                    ])
+  result = Programs.NewtonRaphson._CreateDescentDirection(inverseHessian * gradient)
+  result = result._GetMatrixColumn(1)
+  assert (-1.1 < result[0]) and (result[0] < -0.9) and (-0.1 < result[1]) and (result[1] < 0.1)
   
-# def test_NewtonRaphsonGolden():
-  # startingPoint = [0, 0]
-  # GoalFunction = Tasks.TaskFunctions.f2
-  # PartialDerivativeFunctions = [Tasks.TaskFunctions.df2x0, Tasks.TaskFunctions.df2x1]
-  # result = Programs.NewtonRaphson.NewtonRaphson(startingPoint, GoalFunction, PartialDerivativeFunctions)
-  # assert (3.9 < result[0]) and (result[0] < 4.1) and (1.9 < result[1]) and (result[1] < 2.1)
+def test_NewtonRaphsonGolden():
+  startingPoint = [0, 0]
+  GoalFunction = Tasks.TaskFunctions.f2
+  FirstPartialDerivativeFunctions = [Tasks.TaskFunctions.df2x0, Tasks.TaskFunctions.df2x1]
+  HessianPartialDerivativeFunctions = [[Tasks.TaskFunctions.ddf2x0x0, Tasks.TaskFunctions.ddf2x0x1],
+                                      [Tasks.TaskFunctions.ddf2x0x1, Tasks.TaskFunctions.ddf2x1x1],
+                                     ]
+  result = Programs.NewtonRaphson.NewtonRaphson(startingPoint, GoalFunction, FirstPartialDerivativeFunctions, HessianPartialDerivativeFunctions, useGolden=True)
+  result = result._GetMatrixColumn(1)
+  assert (3.9 < result[0]) and (result[0] < 4.1) and (1.9 < result[1]) and (result[1] < 2.1)
   
-# def test_NewtonRaphsonNoGolden():
-  # startingPoint = [0, 0]
-  # GoalFunction = Tasks.TaskFunctions.f2
-  # PartialDerivativeFunctions = [Tasks.TaskFunctions.df2x0, Tasks.TaskFunctions.df2x1]
-  # result = Programs.NewtonRaphson.NewtonRaphson(startingPoint, GoalFunction, PartialDerivativeFunctions, useGolden=False)
-  # assert (3.9 < result[0]) and (result[0] < 4.1) and (1.9 < result[1]) and (result[1] < 2.1)
+def test_NewtonRaphsonNoGolden():
+  startingPoint = [0, 0]
+  GoalFunction = Tasks.TaskFunctions.f2
+  FirstPartialDerivativeFunctions = [Tasks.TaskFunctions.df2x0, Tasks.TaskFunctions.df2x1]
+  HessianPartialDerivativeFunctions = [[Tasks.TaskFunctions.ddf2x0x0, Tasks.TaskFunctions.ddf2x0x1],
+                                      [Tasks.TaskFunctions.ddf2x0x1, Tasks.TaskFunctions.ddf2x1x1],
+                                     ]
+  result = Programs.NewtonRaphson.NewtonRaphson(startingPoint, GoalFunction, FirstPartialDerivativeFunctions, HessianPartialDerivativeFunctions, useGolden=False)
+  result = result._GetMatrixColumn(1)
+  assert (3.9 < result[0]) and (result[0] < 4.1) and (1.9 < result[1]) and (result[1] < 2.1)
