a4c577bf9adfcc158d4b0f75e3673c15a7eb587a
MislavJaksic <jaksicmislav@gmail.com>
2017-12-04 21:44:27 +0100

    Started APR_Lab4

---
+++ APR/Labs/Lab_4/SolveTasks.py
@@ -0,0 +1,161 @@
+from Programs.Box import Box
+from Programs.ConstraintOptimization import ConstraintOptimization
+from Programs.NewtonRaphson import NewtonRaphson
+from Programs.SteepestDescent import SteepestDescent
+
+from Tasks.TaskFunctions import *
+
+from copy import copy
+
+def TaskOne():
+  x0 = [0, 0]
+  F = f3
+  dF = [df3x0, df3x1]
+  
+  SolveWithSteepestDescent(x0, F, dF, True)
+  SolveWithSteepestDescent(x0, F, dF, False)
+  
+def TaskTwo():
+  x0 = [-1.9, 2]
+  F = f1
+  dF = [df1x0, df1x1]
+  ddF = [[ddf1x0x0, ddf1x0x1],
+         [ddf1x0x1, ddf1x1x1],
+        ]
+  
+  #SolveWithSteepestDescent(x0, F, dF, True)
+  #SolveWithNewtonRaphson(x0, F, dF, ddF, True)
+  
+  #x0 = [0.1, 0.3]
+  x0 = [0, 0]
+  F = f2
+  dF = [df2x0, df2x1]
+  ddF = [[ddf2x0x0, ddf2x0x1],
+         [ddf2x0x1, ddf2x1x1],
+        ]
+  
+  SolveWithSteepestDescent(x0, F, dF, True)
+  #SolveWithNewtonRaphson(x0, F, dF, ddF, True)
+  
+def TaskThree():
+  x0 = [-1.9, 2]
+  F = f1
+  Inequalities = [constraint1, constraint2]
+  Interval = [-100, 100]
+  
+  #SolveWithBox(x0, F, Inequalities, Interval)
+  
+  x0 = [0.1, 0.3]
+  F = f2
+  Inequalities = [constraint1, constraint2]
+  Interval = [-100, 100]
+  
+  SolveWithBox(x0, F, Inequalities, Interval)
+  
+def TaskFour():
+  x0 = [-1.9, 2]
+  F = f1
+  Inequalities = [constraint1, constraint2]
+  
+  #SolveWithConstraintOptimization(x0, F, Inequalities, [])
+  
+  betterx0 = [3, 3]
+  
+  #SolveWithConstraintOptimization(betterx0, F, Inequalities, [])
+  
+  x0 = [0.1, 0.3]
+  F = f2
+  Inequalities = [constraint1, constraint2]
+  
+  SolveWithConstraintOptimization(x0, F, Inequalities, [])
+  
+def TaskFive():
+  x0 = [0, 0]
+  F = f4
+  Inequalities = [constraint3, constraint4]
+  Equalities = [constraint5]
+  
+  #SolveWithConstraintOptimization(x0, F, Inequalities, Equalities)
+  
+  x0 = [5, 5]
+  
+  SolveWithConstraintOptimization(x0, F, Inequalities, Equalities)
+  
+def SolveWithSteepestDescent(x0, function, dF, useGolden):
+  F = CountInvocations(function)
+  newdF = []
+  for f in dF:
+    newdF.append(CountInvocations(f))
+  
+  minimum = SteepestDescent(copy(x0), F, newdF, useGolden)
+  print "SteepestDescent ",
+  PrintMinimumAndInvocations(minimum, F, newdF, [])
+  
+def SolveWithNewtonRaphson(x0, function, dF, ddF, useGolden):
+  F = CountInvocations(function)
+  newdF = []
+  for f in dF:
+    newdF.append(CountInvocations(f))
+  
+  newddF = []
+  for list in ddF:
+    newList = []
+    for f in list:
+      newList.append(CountInvocations(f))
+    newddF.append(newList)
+    
+  minimum = NewtonRaphson(copy(x0), F, newdF, newddF, useGolden=useGolden)
+  print "NewtonRaphson ",
+  PrintMinimumAndInvocations(minimum, F, newdF, newddF)
+  
+def SolveWithBox(x0, function, Inequalities, Interval):
+  F = CountInvocations(function)
+  minimum = Box(copy(x0), F, Inequalities, Interval)
+  print "Box ",
+  PrintMinimumAndInvocations(minimum, F, [], [])
+  
+def SolveWithConstraintOptimization(x0, function, Inequalities, Equalities):
+  F = CountInvocations(function)
+  minimum = ConstraintOptimization(copy(x0), F, Inequalities, Equalities)
+  print "ConstraintOptimization ",
+  PrintMinimumAndInvocations(minimum, F, [], [])
+  
+def PrintMinimumAndInvocations(minimum, F, dF, ddF):
+  print "minimum:",
+  print minimum
+  print "Goal function value at minimum:",
+  print F(minimum)
+  print "Number of goal function invocations:",
+  print F.invocations
+  
+  sum = 0
+  for f in dF:
+    sum+= f.invocations
+  print "Number of first partial function invocations:",
+  print sum
+  
+  sum = 0
+  for list in ddF:
+    for f in list:
+      sum+= f.invocations
+  print "Number of second partial function invocations:",
+  print sum
+  
+  print "-.-.-.-.-.-.-.-.-.-.-.-.-.-.-"
+  print
+  
+def CountInvocations(function):
+  def interdictor(x):
+    interdictor.invocations += 1
+    result = function(x)
+    return result
+    
+  interdictor.invocations = 0
+  
+  return interdictor
+  
+#TaskOne()
+TaskTwo()
+#TaskThree()
+#TaskFour()
+#TaskFive()
---
+++ APR/Labs/Lab_4/Tasks/TaskFunctions.py
@@ -0,0 +1,94 @@
+"""All functions have a uniform interface, x is a list"""
+
+def f1(x):
+  return 100*((x[1] - (x[0]**2))**2) + ((1 - x[0])**2)
+  
+def df1x0(x):
+  return (400*x[0]**3 - 400*x[0]*x[1] + 2*x[0] - 2)
+  
+def ddf1x0x0(x):
+  return (1200*x[0]**2 - 400*x[1] + 2)
+  
+def df1x1(x):
+  return ((-1)*200*x[0]**2 + 200*x[1])
+  
+def ddf1x1x1(x):
+  return 200
+  
+def ddf1x0x1(x):
+  return ((-1)*400*x[0])
+  
+  
+  
+def f2(x):
+  return ((x[0] - 4)**2) + 4*((x[1] - 2)**2)
+  
+def df2x0(x):
+  return (2*x[0] - 8)
+  
+def ddf2x0x0(x):
+  return (2)
+  
+def df2x1(x):
+  return (8*x[1] - 16)
+  
+def ddf2x1x1(x):
+  return (8)
+  
+def ddf2x0x1(x):
+  return (0)
+  
+  
+  
+def f3(x):
+  return ((x[0] - 2)**2) + ((x[1] + 3)**2)
+  
+def df3x0(x):
+  return (2*x[0] - 4)
+  
+def ddf3x0x0(x):
+  return (2)
+  
+def df3x1(x):
+  return (2*x[1] + 6)
+  
+def ddf3x1x1(x):
+  return (2)
+  
+def ddf3x0x1(x):
+  return (0)
+  
+  
+  
+def f4(x):
+  return ((x[0] - 3)**2) + ((x[1])**2)
+  
+  
+  
+def constraint1(x):
+  return x[1] - x[0]
+  
+def constraint2(x):
+  return 2 - x[0]
+  
+def constraint3(x):
+  return 3 - x[0] - x[1]
+  
+def constraint4(x):
+  return 3 + 1.5 * x[0] - x[1]
+ 
+def constraint5(x):
+  return x[1] - 1
+
+  
+def constraint6(x):
+  return 100 - x[0]
+  
+def constraint7(x):
+  return x[0] + 100
+  
+def constraint8(x):
+  return 100 - x[1]
+  
+def constraint9(x):
+  return x[1] + 100
