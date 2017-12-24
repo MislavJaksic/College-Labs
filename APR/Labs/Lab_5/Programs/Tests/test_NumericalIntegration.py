7e8905f0afddbd6cc9c431310da020f719aedfc2
MislavJaksic <jaksicmislav@gmail.com>
2017-12-24 18:40:36 +0100

    Prepared the numerical integration lab_5

---
+++ APR/Labs/Lab_5/Programs/Tests/test_NumericalIntegration.py
@@ -0,0 +1,19 @@
+from Programs.NumericalIntegration
+from Programs.Helpers.Matrix import Matrix
+
+import pytest
+from copy import copy
+
+
+@pytest.fixture(scope='function')
+def ():
+
+  
+def test_():
+  assert 
+
+
+  
+  
+  
+""""""
