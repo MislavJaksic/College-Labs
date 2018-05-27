1b94882ff3bd7d5610301c9fbdaef90764579480
MislavJaksic <jaksicmislav@gmail.com>
2018-05-27 23:23:18 +0200

    Change in architecture

--- PP/Lab_2/ConnectFour.py
+++ PP/Lab_2/ConnectFour.py
@@ -1,9 +1,19 @@
+from mpi4py import MPI
 from copy import deepcopy
+
+# Installing relevant packages: Anaconda 2.x; conda install mpi4py; works on linux
+
+# http://mpi4py.scipy.org/docs/apiref/index.html
+# http://mpi4py.readthedocs.io/en/stable/intro.html
+
+#Run with: mpiexec -n 2 python ConnectFour.py
 
 BOARD_WIDTH = 7
 COMPUTER_VICTORY = 1
 PLAYER_VICTORY = -1
 NEITHER_WIN = 0
+
+NUMBER_OF_COMPUTER_MOVES = 3 # >3 can take a huge amount of time!
 
 class ConnectFourGrid(object):
   def __init__(self):
@@ -212,14 +222,38 @@ class ConnectFourGrid(object):
     
     
 def CalculateTopStateValue(top_state):
-  computer_states, player_computer_states = ConstructStates(top_state)
-  computer_values, player_values = AssignValuesToStates(computer_states, player_computer_states)
-  top_value = PropagateStatesUpwards(top_state, computer_values, player_values)
+  all_state_tiers = CreateStatesToDesiredDepth(top_state)
+  # for tier in all_state_tiers:
+    # print len(tier)
+  all_state_values = AssignValuesToStates(all_state_tiers)
+  # for values in all_state_values:
+    # print len(values)
+  top_value = PropagateStatesUpwards(top_state, all_state_values)
   
   print top_value
   
   return top_value
   
+  
+def CreateStatesToDesiredDepth(top_state):
+  branching_states = [top_state]
+  all_states =[]
+  for depth in range(NUMBER_OF_COMPUTER_MOVES - 1):
+  
+    computer_tier = []
+    player_tier = []
+    for branching_state in branching_states:
+      computer_states, player_after_computer_states = ConstructStates(branching_state)
+      computer_tier.extend(computer_states)
+      player_tier.extend(player_after_computer_states)
+    
+    all_states.append(computer_tier)
+    all_states.append(player_tier)
+    
+    branching_states = player_tier
+    
+  #all_states = reversed(all_states)
+  return all_states
   
 def ConstructStates(top_state):
   computer_states = SimulateComputerMoves(top_state)
@@ -250,11 +284,19 @@ def SimulatePlayerMoves(computer_states):
   return player_computer_states
 
   
-def AssignValuesToStates(computer_states, player_computer_states):
-  computer_values = AssignComputerMoveValues(computer_states)
-  player_values = AssignPlayerMoveValues(player_computer_states)
+def AssignValuesToStates(all_state_tiers):
+  all_state_values = []
+  for depth in range(NUMBER_OF_COMPUTER_MOVES - 1):
+    computer_states = all_state_tiers[depth*2]
+    player_after_computer_states = all_state_tiers[depth*2 + 1]
+    
+    computer_values = AssignComputerMoveValues(computer_states)
+    player_values = AssignPlayerMoveValues(player_after_computer_states)
+    
+    all_state_values.append(computer_values)
+    all_state_values.append(player_values)
   
-  return computer_values, player_values
+  return all_state_values
   
 def AssignComputerMoveValues(computer_states):
   computer_values = []
@@ -277,25 +319,33 @@ def AssignPlayerMoveValues(player_states):
   return player_values
  
   
-def PropagateStatesUpwards(top_state, computer_values, player_values):
-  propagated_values = PropagatePlayerValuesUpwards(computer_values, player_values)
+def PropagateStatesUpwards(top_state, all_state_values):
+  all_state_values.reverse()
   
-  # for state in computer_states:
-    # state.Print()
-  # print propagated_values
+  player_values = all_state_values[0]
+  computer_values = all_state_values[1]
+  propagated_computer_values = PropagatePlayerValuesUpwards(computer_values, player_values)
   
-  top_value = PropagateComputerValuesUpwards(top_state, propagated_values)
+  for depth in range(1, NUMBER_OF_COMPUTER_MOVES - 1):
+    player_values = all_state_values[depth*2]
+    computer_values = all_state_values[depth*2 + 1]
+    
+    propagated_player_values = PropagateComputerValuesUpwards(propagated_computer_values, player_values)
+    propagated_computer_values = PropagatePlayerValuesUpwards(computer_values, propagated_player_values)
+    
+  player_values = [top_state.CheckVictory()]
+  top_value = PropagateComputerValuesUpwards(propagated_computer_values, player_values)
   
   return top_value
   
 def PropagatePlayerValuesUpwards(computer_values, player_values):
   propagated_values = []
-  for number in range(BOARD_WIDTH):
+  for number in range(len(computer_values)):
     computer_move_value = computer_values[number]
     
     if computer_move_value == NEITHER_WIN:
       relevant_player_values = player_values[number*BOARD_WIDTH:(number+1)*BOARD_WIDTH]
-      value = CalculateUpPropagationValue(relevant_player_values)
+      value = CalculateComputerUpPropagationValue(relevant_player_values)
       propagated_values.append(value)
     elif computer_move_value == COMPUTER_VICTORY:
       propagated_values.append(computer_move_value)
@@ -304,7 +354,7 @@ def PropagatePlayerValuesUpwards(computer_values, player_values):
       
   return propagated_values
   
-def CalculateUpPropagationValue(player_values):
+def CalculateComputerUpPropagationValue(player_values):
   value_sum = 0
   for value in player_values:
     if value != PLAYER_VICTORY:
@@ -314,35 +364,84 @@ def CalculateUpPropagationValue(player_values):
       
   return float(value_sum) / BOARD_WIDTH
   
-def PropagateComputerValuesUpwards(top_state, propagated_values):
-  if top_state.CheckVictory() == True:
-    return PLAYER_VICTORY
-  
-  value_sum = 0  
-  for value in propagated_values:
-    # if value != COMPUTER_VICTORY:
-      # value_sum += value
-    # else:
-      # return COMPUTER_VICTORY
-    value_sum += value
+def PropagateComputerValuesUpwards(computer_values, player_values):
+  propagated_values = []
+  for number in range(len(player_values)):
+    player_move_value = player_values[number]
+    
+    if player_move_value == NEITHER_WIN:
+      relevant_computer_values = computer_values[number*BOARD_WIDTH:(number+1)*BOARD_WIDTH]
+      value = CalculatePlayerUpPropagationValue(relevant_computer_values)
+      propagated_values.append(value)
+    elif player_move_value == PLAYER_VICTORY:
+      propagated_values.append(player_move_value)
+    else:
+      raise Exception("ERROR")
       
+  return propagated_values
+      
+def CalculatePlayerUpPropagationValue(computer_values):
+  value_sum = 0
+  for value in computer_values:
+    if value != COMPUTER_VICTORY:
+      value_sum += value
+    else:
+      return COMPUTER_VICTORY
       
   return float(value_sum) / BOARD_WIDTH
       
+
+      
+def ComputerThinks(communicator, current_state):
+  computer_states, player_computer_states = ConstructStates(current_state)
+  tasks = player_computer_states
   
- 
- 
-state = ConnectFourGrid()
-state.AddTokenToColumn("C", 2)
-state.AddTokenToColumn("C", 2)
-state.AddTokenToColumn("C", 2)
-state.AddTokenToColumn("C", 3)
-state.AddTokenToColumn("C", 3)
-state.AddTokenToColumn("C", 4)
-state.AddTokenToColumn("P", 2)
-state.AddTokenToColumn("P", 3)
-state.AddTokenToColumn("P", 4)
-CalculateTopStateValue(state)
+  for state in player_computer_states:
+    results = DistributeTasksAndRecieveResults(task_tier)
+
+  
+
+
+
+def DistributeTasksAndRecieveResults(task_tier):
+  for state in task_tier:
+    pass
+  #communicator.send(message, dest=)
+
+      
+# state = ConnectFourGrid()
+# state.AddTokenToColumn("C", 0)
+# state.AddTokenToColumn("C", 1)
+# state.AddTokenToColumn("P", 5)
+# state.AddTokenToColumn("P", 6)
+# CalculateTopStateValue(state)
+      
+      
+# if __name__ == "__main__":
+  # communicator = MPI.COMM_WORLD
+  # process_rank = communicator.Get_rank()
+  # number_of_processes = communicator.Get_size()
+  
+  # if process_rank == 0:
+    # current_state = ConnectFourGrid()
+    # ComputerThinks(communicator, current_state)
+  # else:
+    # pass
+  
+  #computer thinks
+  #computer makes a move
+  #player makes a move
+
+  #computer think
+    #create states until the desired depth
+    #send tasks to be completed
+    #recieve results
+    #use results and create other tasks
+    #do so until the top
+
+  
+  
+
   
   
   
