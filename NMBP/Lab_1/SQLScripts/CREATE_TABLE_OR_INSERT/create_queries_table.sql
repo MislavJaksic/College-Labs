aad2e29abe2985bdfcffee07c791a00352dc63cb
MislavJaksic <jaksicmislav@gmail.com>
2018-10-25 09:10:54 +0200

    Changed quotes

--- NMBP/Lab_1/SQLScripts/CREATE_TABLE_OR_INSERT/create_queries_table.sql
+++ NMBP/Lab_1/SQLScripts/CREATE_TABLE_OR_INSERT/create_queries_table.sql
@@ -5,12 +5,12 @@ CREATE TABLE queries (
 );
 
 INSERT INTO queries (query, date_and_time) VALUES ('SELECT * FROM imaginary_table;', '1999-01-08 04:05:06');
-INSERT INTO queries (query, date_and_time) VALUES ("SELECT * FROM imaginary_table;", "1999-01-10 04:05:06");
-INSERT INTO queries (query, date_and_time) VALUES ("SELECT * FROM imaginary_table;", "1999-01-11 04:05:06");
-INSERT INTO queries (query, date_and_time) VALUES ("SELECT * FROM imaginary_table;", "1999-01-11 06:05:06");
-INSERT INTO queries (query, date_and_time) VALUES ("SELECT * FROM imaginary_table;", "1999-01-11 07:05:06");
-INSERT INTO queries (query, date_and_time) VALUES ("SELECT * FROM imaginary_table;", "1999-01-11 10:05:06");
-INSERT INTO queries (query, date_and_time) VALUES ("SELECT a_column FROM another_table;", "1999-01-09 05:05:06");
-INSERT INTO queries (query, date_and_time) VALUES ("SELECT a_column FROM another_table;", "1999-01-11 09:05:06");
-INSERT INTO queries (query, date_and_time) VALUES ("SELECT a_column FROM another_table;", "1999-01-10 08:05:06");
+INSERT INTO queries (query, date_and_time) VALUES ('SELECT * FROM imaginary_table;', '1999-01-10 04:05:06');
+INSERT INTO queries (query, date_and_time) VALUES ('SELECT * FROM imaginary_table;', '1999-01-11 04:05:06');
+INSERT INTO queries (query, date_and_time) VALUES ('SELECT * FROM imaginary_table;', '1999-01-11 06:05:06');
+INSERT INTO queries (query, date_and_time) VALUES ('SELECT * FROM imaginary_table;', '1999-01-11 07:05:06');
+INSERT INTO queries (query, date_and_time) VALUES ('SELECT * FROM imaginary_table;', '1999-01-11 10:05:06');
+INSERT INTO queries (query, date_and_time) VALUES ('SELECT a_column FROM another_table;', '1999-01-09 05:05:06');
+INSERT INTO queries (query, date_and_time) VALUES ('SELECT a_column FROM another_table;', '1999-01-11 09:05:06');
+INSERT INTO queries (query, date_and_time) VALUES ('SELECT a_column FROM another_table;', '1999-01-10 08:05:06');
 
