2c3f01509386132ee029f47724db62decbf61f34
MislavJaksic <jaksicmislav@gmail.com>
2018-12-09 08:14:00 +0100

    Added finishing touches to Lab_2 RASUS

--- RASSUS/Lab_2/distributed_system_node/distributed_system_node/src/main/java/mjaksic/distributed_system_node/RunnerTwo.java
+++ RASSUS/Lab_2/distributed_system_node/distributed_system_node/src/main/java/mjaksic/distributed_system_node/RunnerTwo.java
@@ -10,7 +10,7 @@ public class RunnerTwo {
 		ArrayList<Integer> ports = new ArrayList<Integer>();
 		ports.add(10001);
 		ports.add(10000);
-		//ports.add(10002);
+		ports.add(10002);
 		
 		Sensor sensor_A = new Sensor(ports);
 	}
